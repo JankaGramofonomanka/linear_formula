c44d04fba12ebf0eff1aa6aa1198e400ec11c32b
JankaGramofonomanka <tsrn666#gmail.com>
2020-03-09 16:45:33 +0100

    Modify test_linear_formula.py
    Modified test_linear_formula.py such that it will perform tests when
    called from outside

--- test_linear_formula.py
+++ test_linear_formula.py
@@ -1,11 +1,9 @@
 import unittest
-import linear_formula as lf
-from linear_formula import LinearFormula
+from test_init import TestInit
+from test_magic_methods import TestMagicMethods
+from test_modifiers import TestModifiers
+from test_other import TestOther
 
 if __name__ == '__main__':
-    from test_init import TestInit
-    from test_magic_methods import TestMagicMethods
-    from test_modifiers import TestModifiers
-    from test_other import TestOther
 
     unittest.main()
